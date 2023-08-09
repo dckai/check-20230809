b8cc8fd0ebdd2bbc48898d55bd29086694298aa6
dck <dck1997@qq.com>
2023-08-09 09:17:52 +0800

    'fix'

--- checkin.sh
+++ checkin.sh
@@ -1,5 +1,5 @@
 echo '------------------sign------------------'
-curl -H "cookie:${COOKIE}" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.rocks"}' -X POST 'https://glados.rocks/api/user/checkin' | grep -Eo '"message":"[^"]*"'
+curl -H "cookie:${COOKIE}" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.one"}' -X POST 'https://glados.rocks/api/user/checkin' | grep -Eo '"message":"[^"]*"'
 echo '-----------------status-----------------'
 curl -H "cookie:${COOKIE}" -X GET 'https://glados.rocks/api/user/status' | grep -Eo '"leftDays":"[^"]*"'
 
